7c3e550191ffe60bb4ebfcc81e0c822b2be02a2c
Gregoire <gregoire.haardt@epitech.eu>
2020-05-11 12:27:55 +0200

    ADD: implementation of base 64 encoding algorithm
    Also removed print statement that displayed filename and mode.

--- src/base64.c
+++ src/base64.c
@@ -2,7 +2,6 @@
 
 int base64(char const *filename, int mode)
 {
-    printf("filename: '%s', mode = %s\n", filename, mode == 0 ? "ENCODE" : "DECODE");
     if (mode == ENCODE)
         return encode(filename);
     else
